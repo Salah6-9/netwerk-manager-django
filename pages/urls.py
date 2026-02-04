2e849e6e314c05d8aff2b675f02938ec0dd51bc6
Salah <wirxe_salah@protonmail.com>
2026-02-05 00:54:23 +0100

    Add secured dashboard page

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from . import views
+from .views import dashboard
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Root URL mapped to index view
-    path('about', views.aboutme, name='aboutme'),  # /about/ URL mapped to about view
-    path('contact', views.contact, name='contact'),  # /contact/ URL mapped to contact view
-
+    path("dashboard/", dashboard, name="dashboard"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,25 +1,7 @@
 from django.shortcuts import render
-from . models import Contact
-from .forms import ContactForm
-#from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
-def home(request):
-    context = { 'name01': 'SOOL', 'name02': 'ALI' }
-    return render(request, 'pages/home.html', context)
 
-def aboutme(request):
-    return render(request, 'pages/about.html')
-
-def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return render(request, 'pages/contact.html', {'form': ContactForm(), 'success': True})
-    else:
-        form = ContactForm()
-    return render(request, 'pages/contact.html', {'form': form})
-
-    
- 
+@login_required
+def dashboard(request):
+    return render(request, "dashboard.html")
