1d62f2b63c2737fea4defa6def902362d72c1052
DJILALI SALAH EDDINE <91536209+Salah6-9@users.noreply.github.com>
2026-03-08 00:44:17 +0100

    Revert "Implement Metrics Ingestion "

--- devices/models.py
+++ devices/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-import secrets
 
 class Device(models.Model):
     STATUS_CHOICES = [
@@ -8,35 +7,12 @@ class Device(models.Model):
         ("offline", "Offline"),
         ("unknown", "Unknown"),
     ]
-    office = models.ForeignKey(
-        "users.Office",
-        on_delete=models.PROTECT,
-        null=True,
-        blank=True
-    )
-    user = models.ForeignKey(
-        User, null= True,blank=True,
-        on_delete=models.SET_NULL, 
-        related_name="devices"
-    )
+
+    user = models.ForeignKey(User, null= True,on_delete=models.CASCADE, related_name="devices")
     ip = models.GenericIPAddressField()
-    mac = models.CharField(
-        max_length=100, unique=True
-    )
-    status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default="offline"
-    )
-    device_number = models.CharField(max_length=100, blank=True)
-    is_active = models.BooleanField(default=True)
-    agent_token = models.CharField(
-        max_length=128, blank=True,
-        null=True, unique=True
-    )
-    def save(self, *args, **kwargs):
-        if not self.agent_token:
-            self.agent_token = secrets.token_hex(32)
-        super().save(*args, **kwargs)
-    
+    mac = models.CharField(max_length=100, unique=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="offline")
+
     def __str__(self):
         if self.user:
             return f"{self.user.username} - {self.mac}"
--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from .views import dashboard
-from .views import dashboard_status_api
 from .import views
 
 
 urlpatterns = [
     path("dashboard/", dashboard, name="dashboard"),
     path("trigger-scan/", views.trigger_scan, name="trigger_scan"),
-    path("api/dashboard/status/", dashboard_status_api, name="dashboard_status_api"),
 ]
