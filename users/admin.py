cee29425236a661d84731e92b46dc50fdacfa893
Salah <wirxe_salah@protonmail.com>
2026-03-08 05:15:37 +0100

    fix: restore DB v2 architecture after git recovery (Department/Office, RBAC, ScanRun tracking, Client removal)

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Department, Office, Profile 
+from .models import Profile, Department, Office
 
-admin.site.register(Department)
-admin.site.register(Office)
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "office")
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Office)
+class OfficeAdmin(admin.ModelAdmin):
+    list_display = ("name", "department")
