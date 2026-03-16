a81b7c522660223ee89e68da437cdaf4d0ee4fda
Salah <wirxe_salah@protonmail.com>
2026-03-16 14:57:47 +0100

    Fix Tailwind configuration, enable ManifestStaticFilesStorage, and stabilize base layout

--- project/settings.py
+++ project/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
